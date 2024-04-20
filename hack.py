4cf84af2dba8d9e7454526b6ed0a102a27a8916b
idk <107901430+idk691@users.noreply.github.com>
2024-04-20 23:46:30 +0700

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,21 @@
+import time
+import random
+
+def hacking_progress():
+    print("Hacking in progress:")
+    for i in range(101):
+        print(f"Progress: {i}% ", end="", flush=True)
+        time.sleep(random.uniform(0.1, 0.5))
+        if i < 30:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+        elif i < 70:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||          ]", end="\r", flush=True)
+        elif i < 90:
+            print("[||||||||||||||||||||||||||||||||||||||||||||                    ]", end="\r", flush=True)
+        elif i < 100:
+            print("[||||||||||||||||||||||||||||||||||||                            ]", end="\r", flush=True)
+        else:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+    print("\nHack successful!")
+
+hacking_progress()
