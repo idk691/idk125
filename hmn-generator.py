289891d71e3121ef878fe9955084ffc6831dd897
idk <107901430+idk691@users.noreply.github.com>
2024-04-23 11:56:20 +0700

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,5 +1,29 @@
-import requests
 import time
+import requests
+
+def hacking_progress():
+    print("VPN connection in progress:")
+    start_time = time.time()
+    for i in range(101):
+        if i >= 100:
+            i = 100
+        print(f"Progress: {i}% ", end="", flush=True)
+        remaining_time = 10 - (time.time() - start_time)
+        if remaining_time > 0:
+            time.sleep(remaining_time / (100 - i))
+        if i < 30:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+        elif i < 70:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||          ]", end="\r", flush=True)
+        elif i < 90:
+            print("[||||||||||||||||||||||||||||||||||||||||||||                    ]", end="\r", flush=True)
+        elif i < 99:
+            print("[||||||||||||||||||||||||||||||||||||                            ]", end="\r", flush=True)
+        else:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+    print("\nVPN connection successful!")
+
+hacking_progress()
 
 url = 'https://hidemy.io/ru/demo/'
 
