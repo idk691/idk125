001a578af8c6732d0f69c86029cbb3d520a8d8a7
idk <107901430+idk691@users.noreply.github.com>
2024-04-23 11:47:32 +0700

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,13 +1,41 @@
-import sys
+import requests
 import time
 
-def loading_animation(duration):
-    while duration > 0:
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        time.sleep(1)
-        duration -= 1
+url = 'https://hidemy.io/ru/demo/'
 
-print("Подключение к серверу", end="")
-loading_animation(10)
-print("\nНе удалось подключить к серверу")
+print("Загрузка страницы: ", end="")
+for i in range(101):
+    print(f"\rЗагрузка страницы: {i}% ", end="")
+    time.sleep(0.03)
+print("\nЗагрузка завершена.")
+
+response = requests.get(url)
+
+if response.ok:
+    if 'Ваша электронная почта' in response.text:
+        email = input('Введите электронную почту для получения тестового периода: ')
+
+        response = requests.post('https://hidemy.io/ru/demo/success/', data={
+            "demo_mail": f"{email}"
+        })
+
+        if 'Ваш код выслан на почту' in response.text:
+            confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
+            
+            while True:
+                try:
+                    response = requests.get(confirm)
+                    if 'Спасибо' in response.text:
+                        print('Почта подтверждена. Код отправлен на вашу почту!')
+                        break
+                    else:
+                        confirm = input('Ссылка невалидная, повторите попытку: ')
+                except:
+                    confirm = input('Ссылка невалидная, повторите попытку: ')
+                    continue
+        else:
+            print('Указанная почта не подходит для получения тестового периода ')
+    else:
+        print('Страница не содержит форму для ввода электронной почты')
+else:
+    print('Ошибка при загрузке страницы')
