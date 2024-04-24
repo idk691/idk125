6fb562eb169e796f568bcb59e1ea63dd51437af6
idk <107901430+idk691@users.noreply.github.com>
2024-04-24 08:04:07 +0700

    Update status.py

--- status.py
+++ status.py
@@ -1,8 +1,8 @@
 all_statuses = [
-    "✅Server",
-    "✅Bypass [Premium]",
-    "✅Website",
-    "✅VPN hidemyname"
+    "✅Server (0 ошибок)",
+    "✅Bypass [Premium] (0 ошибок)",
+    "✅Website (0 ошибок)",
+    "✅VPN hidemyname (0 ошибок)"
 ]
 
 print("status:")
