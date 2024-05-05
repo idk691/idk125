75357ae0ac3ad717060ae085842ff9eb89e1f9ac
idk <107901430+idk691@users.noreply.github.com>
2024-05-05 18:42:44 +0700

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,20 +1,28 @@
-for i in range(101):
-    if i >= 100:
-        i = 100
-    print(f"Progress: {i}% ", end="", flush=True)
-    remaining_time = 10 - (time.time() - start_time)
-    if remaining_time > 0:
-        time.sleep(remaining_time / (100 - i))
-    if i < 30:
-        print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
-    elif i < 70:
-        print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||          ]", end="\r", flush=True)
-    elif i < 90:
-        print("[||||||||||||||||||||||||||||||||||||||||||||                    ]", end="\r", flush=True)
-    elif i < 99:
-        print("[||||||||||||||||||||||||||||||||||||                            ]", end="\r", flush=True)
-    else:
-        print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
-    if i == 100:
-        print("\nFailed to connect to VPN. Exiting script.")
-        exit()
+import time
+import requests
+
+def hacking_progress():
+    print("VPN connection in progress:")
+    start_time = time.time()
+    for i in range(101):
+        if i >= 100:
+            i = 100
+        print(f"Progress: {i}% ", end="", flush=True)
+        remaining_time = 10 - (time.time() - start_time)
+        if remaining_time > 0:
+            time.sleep(remaining_time / (100 - i))
+        if i < 30:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+        elif i < 70:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||          ]", end="\r", flush=True)
+        elif i < 90:
+            print("[||||||||||||||||||||||||||||||||||||||||||||                    ]", end="\r", flush=True)
+        elif i < 99:
+            print("[||||||||||||||||||||||||||||||||||||                            ]", end="\r", flush=True)
+        else:
+            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
+        if i == 100:
+            print("\nFailed to connect to VPN. Exiting script.")
+            exit()
+
+hacking_progress()
