f4e597091fe605e1d156b86b243b0ccbf4995e03
idk <107901430+idk691@users.noreply.github.com>
2024-05-11 18:11:33 +0700

    Update hmn-generator.py

--- hmn-generator.py
+++ hmn-generator.py
@@ -1,28 +1,38 @@
-import time
 import requests
+import random
+import string
 
-def hacking_progress():
-    print("VPN connection in progress:")
-    start_time = time.time()
-    for i in range(101):
-        if i >= 100:
-            i = 100
-        print(f"Progress: {i}% ", end="", flush=True)
-        remaining_time = 10 - (time.time() - start_time)
-        if remaining_time > 0:
-            time.sleep(remaining_time / (100 - i))
-        if i < 30:
-            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
-        elif i < 70:
-            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||          ]", end="\r", flush=True)
-        elif i < 90:
-            print("[||||||||||||||||||||||||||||||||||||||||||||                    ]", end="\r", flush=True)
-        elif i < 99:
-            print("[||||||||||||||||||||||||||||||||||||                            ]", end="\r", flush=True)
-        else:
-            print("[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||]", end="\r", flush=True)
-        if i == 100:
-            print("\nFailed to connect to VPN. Exiting script.")
-            exit()
+# Генерация случайного ID
+random_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
 
-hacking_progress()
+url = f'https://hidemy.io/ru/demo/{random_id}/'
+
+if 'Ваша электронная почта' in requests.get(url).text:
+    
+    email = input('Введите электронную почту для получения тестового периода: ')
+
+    response = requests.post(f'https://hidemy.io/ru/demo/{random_id}/success/', data={
+        "demo_mail": f"{email}"
+    })
+
+    if 'Ваш код выслан на почту' in response.text:
+        confirm = input('Введите полученную ссылку для подтверждения e-mail адреса: ')
+        
+        while True:
+            try:
+                response = requests.get(confirm)
+                if 'Спасибо' in response.text:
+                    print('Почта подтверждена. Код отправлен на вашу почту!')
+                    break
+                else:
+                    confirm = input('Ссылка невалидная, повторите попытку: ')
+            except:
+                confirm = input('Ссылка невалидная, повторите попытку: ')
+                continue
+
+
+    else:
+        print('Указанная почта не подходит для получения тестового периода ')
+
+else:
+    print('Невозможно получить тестовый период')
